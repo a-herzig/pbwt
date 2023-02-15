9b7a3f1044127e8b672c3c76246d7964487b3c0a
a-herzig <anthony.herzig@inserm.fr>
2023-02-15 14:47:47 +0100

    Update pbwtPaint.c

--- pbwtPaint.c
+++ pbwtPaint.c
@@ -53,7 +53,7 @@ static inline void printAll(int ii,int Ninds,
   gzprintf (fr,"IND%i %.2f\n",ii+1, nregions) ; 
 }
 
-void paintAncestryMatrix (PBWT *p, char* fileRoot,int chunksperregion,int ploidy, int L1, int L2)
+void paintAncestryMatrix (PBWT *p, char* fileRoot,int chunksperregion,int ploidy, int L)
 {
   int Ninds=p->M/ploidy;
   double **totlengths = 0 ;
@@ -81,7 +81,7 @@ void paintAncestryMatrix (PBWT *p, char* fileRoot,int chunksperregion,int ploidy
 
   maxMatch = myalloc (p->M, Array) ;
   for (i = 0 ; i < p->M ; ++i) maxMatch[i] = arrayCreate (1024, MatchSegment) ;
-  matchLongWithin3 (p,L1,L2, reportMatch) ;  /* store maximal matches in maxMatch */
+  matchLongWithin2 (p,L1, reportMatch) ;  /* store maximal matches in maxMatch */
   double *partCounts = myalloc (Ninds, double) ;
   /* now weight per site based on distance from ends */
 
